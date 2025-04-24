fff45cfe01896d5afd60f5967b67ec1f809f1ff6
Metalit <48568911+Metalit@users.noreply.github.com>
2025-04-24 10:41:03 -0600

    Add enum operators

--- shared/operators.hpp
+++ shared/operators.hpp
@@ -18,17 +18,6 @@
         return T::op_##name(lhs, rhs);                    \
     }
 
-#define CREATE_UNARY_OPERATOR(symbol, name) \
-    template <typename T>                   \
-    concept HasOp##name = requires(T a) {   \
-        T::op_##name(a);                    \
-    };                                      \
-    template <typename T>                   \
-    requires HasOp##name<T>                 \
-    auto operator symbol(const T& obj) {    \
-        return T::op_##name(obj);           \
-    }
-
 CREATE_BINARY_OPERATOR(==, Equality)
 CREATE_BINARY_OPERATOR(!=, Inequality)
 CREATE_BINARY_OPERATOR(>, GreaterThan)
@@ -41,7 +30,52 @@ CREATE_BINARY_OPERATOR(-, Subtraction)
 CREATE_BINARY_OPERATOR(*, Multiply)
 CREATE_BINARY_OPERATOR(/, Division)
 
-CREATE_UNARY_OPERATOR(-, UnaryNegation)
-
 #undef CREATE_BINARY_OPERATOR
+
+#define CREATE_UNARY_OPERATOR(symbol, name) \
+    template <typename T>                   \
+    concept HasOp##name = requires(T a) {   \
+        T::op_##name(a);                    \
+    };                                      \
+    template <typename T>                   \
+    requires HasOp##name<T>                 \
+    auto operator symbol(const T& obj) {    \
+        return T::op_##name(obj);           \
+    }
+
+CREATE_UNARY_OPERATOR(-, Negation)
+
 #undef CREATE_UNARY_OPERATOR
+
+template <typename T>
+concept IsCordlEnum = std::is_integral_v<typename T::__CORDL_BACKING_ENUM_TYPE> &&
+                      requires(T t) { static_cast<T>(static_cast<typename T::__CORDL_BACKING_ENUM_TYPE>(t)); };
+
+#define CREATE_BINARY_ENUM_OPERATOR(symbol)                            \
+template <class T>                                                     \
+requires IsCordlEnum<T>                                                \
+T operator symbol(T const& lhs, T const& rhs) {                        \
+    return static_cast<T>(                                             \
+        static_cast<typename T::__CORDL_BACKING_ENUM_TYPE>(lhs) symbol \
+        static_cast<typename T::__CORDL_BACKING_ENUM_TYPE>(rhs)        \
+    );                                                                 \
+}
+
+CREATE_BINARY_ENUM_OPERATOR(&)
+CREATE_BINARY_ENUM_OPERATOR(|)
+CREATE_BINARY_ENUM_OPERATOR(^)
+
+#undef CREATE_BINARY_ENUM_OPERATOR
+
+#define CREATE_UNARY_ENUM_OPERATOR(symbol)                             \
+template <class T>                                                     \
+requires IsCordlEnum<T>                                                \
+T operator symbol(T const& obj) {                                      \
+    return static_cast<T>(                                             \
+        symbol static_cast<typename T::__CORDL_BACKING_ENUM_TYPE>(obj) \
+    );                                                                 \
+}
+
+CREATE_UNARY_ENUM_OPERATOR(~)
+
+#undef CREATE_UNARY_ENUM_OPERATOR
